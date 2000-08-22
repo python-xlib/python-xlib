1b102632749a548f9a05757d8ce1ea82f3d64623
Peter Liljenberg <peter.liljenberg@gmail.com>
2000-08-22 14:06:22 +0000

    Drop circular references in extensions, prepare for 0.3.

--- Xlib/XK.py
+++ Xlib/XK.py
@@ -1,6 +1,22 @@
+# $Id: XK.py,v 1.2 2000-08-22 14:06:22 petli Exp $
 #
-# $Id: XK.py,v 1.1 2000-08-22 13:53:01 petli Exp $
+# Xlib.XK -- X keysym defs
 #
+#    Copyright (C) 2000 Peter Liljenberg <petli@ctrl-c.liu.se>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 XK_BackSpace = 0xFF08
 XK_Tab = 0xFF09
