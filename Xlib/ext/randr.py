739b5aba66fbefcd5d72ad11ce18995ae6ac0a04
mggrant <mgg@nobodymuch.org>
2010-07-19 00:31:33 +0000

    mggr: get rid of annoying "Xlib.protocol.request.QueryExtension" printout

--- Xlib/ext/randr.py
+++ Xlib/ext/randr.py
@@ -1139,8 +1139,6 @@ class OutputPropertyNotify(rq.Event):
 # Initialization #
 
 def init(disp, info):
-    print info.__class__
-
     disp.extension_add_method('display', 'xrandr_query_version', query_version)
     disp.extension_add_method('window', 'xrandr_select_input', select_input)
     disp.extension_add_method('window', 'xrandr_get_screen_info', get_screen_info)
