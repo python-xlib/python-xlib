899cf08b6671765f7e141d7595570ab8245cb536
forestatq <forestatq@0d99f19b-8f59-4e52-b65c-84e472e066d9>
2011-07-31 19:30:08 +0000

    Add a partial implementation of the SECURITY extension and an example that uses it.

---
+++ trunk/examples/security.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+#
+# examples/security.py -- demonstrate the SECURITY extension
+#
+#    Copyright (C) 2011 Outpost Embedded, LLC
+#      Forest Bond <forest.bond@rapidrollout.com>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+import sys, os
+from optparse import OptionParser
+
+# Change path so we find Xlib
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+from Xlib.display import Display
+from Xlib.ext import security
+
+
+def main(argv):
+    parser = OptionParser()
+
+    parser.add_option('--generate', action='store_true', default=False)
+    parser.add_option('--proto', default='MIT-MAGIC-COOKIE-1')
+    parser.add_option('--trusted', action='store_true', default=False)
+    parser.add_option('--untrusted', action='store_true', default=False)
+
+    parser.add_option('--revoke', action='store_true', default=False)
+
+    opts, args = parser.parse_args(argv[1:])
+
+    if opts.trusted and opts.untrusted:
+        parser.error('--trusted and --untrusted cannot be combined')
+
+    if not any((opts.generate, opts.revoke)):
+        parser.error('specify --generate or --revoke')
+
+    display = Display()
+
+    if not display.has_extension('SECURITY'):
+        if display.query_extension('SECURITY') is None:
+            print >>sys.stderr, 'SECURITY extension not supported'
+            return 1
+
+    security_version = display.security_query_version()
+    print >>sys.stderr, 'Found SECURITY version %s.%s' % (
+      security_version.major_version,
+      security_version.minor_version,
+    )
+
+    if opts.generate:
+        kwargs = {}
+        if opts.trusted:
+            kwargs['trust_level'] = security.SecurityClientTrusted
+        elif opts.untrusted:
+            kwargs['trust_level'] = security.SecurityClientUntrusted
+        reply = display.security_generate_authorization(opts.proto, **kwargs)
+        print reply.authid
+
+    elif opts.revoke:
+        for arg in args:
+            authid = int(arg, 10)
+            display.security_revoke_authorization(authid)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
