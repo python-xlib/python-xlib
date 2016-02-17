87b88cdadc38004413fdd783913be9cdec7d415c
Thomas Kluyver <thomas@kluyver.me.uk>
2016-02-17 22:13:53 +0000

    Use bytes literals in test file

--- test/test_events_le.py
+++ test/test_events_le.py
@@ -11,6 +11,13 @@ import Xlib.protocol.event
 import struct
 import array
 
+if sys.version_info[0] >= 3:
+    def _ordb(i):
+        return i
+else:
+    def _ordb(c):
+        return ord(c)
+
 class CmpArray:
     def __init__(self, *args, **kws):
         self.array = array.array(*args, **kws)
@@ -30,7 +37,7 @@ class CmpArray:
 rq.array = CmpArray
 
 def tohex(bin):
-    bin = string.join(map(lambda c: '\\x%02x' % ord(c), bin), '')
+    bin = ''.join(map(lambda c: '\\x%02x' % _ordb(c), bin))
 
     bins = []
     for i in range(0, len(bin), 16):
@@ -43,7 +50,7 @@ def tohex(bin):
         except IndexError:
             bins2.append("'%s'" % bins[i])
 
-    return string.join(bins2, ' \\\n            ')
+    return ' \\\n            '.join(bins2)
 
 class DummyDisplay:
     def get_resource_class(self, x):
@@ -66,10 +73,10 @@ class TestKeymapNotify(unittest.TestCase):
             'type': 173,
             'data': [130, 181, 177, 244, 167, 144, 216, 185, 228, 220, 254, 130, 239, 213, 142, 240, 233, 248, 161, 238, 160, 205, 212, 205, 166, 156, 241, 169, 198, 147, 144],
             }
-        self.evt_bin_0 = '\xad\x82\xb5\xb1' '\xf4\xa7\x90\xd8' \
-            '\xb9\xe4\xdc\xfe' '\x82\xef\xd5\x8e' \
-            '\xf0\xe9\xf8\xa1' '\xee\xa0\xcd\xd4' \
-            '\xcd\xa6\x9c\xf1' '\xa9\xc6\x93\x90'
+        self.evt_bin_0 = b'\xad\x82\xb5\xb1' b'\xf4\xa7\x90\xd8' \
+            b'\xb9\xe4\xdc\xfe' b'\x82\xef\xd5\x8e' \
+            b'\xf0\xe9\xf8\xa1' b'\xee\xa0\xcd\xd4' \
+            b'\xcd\xa6\x9c\xf1' b'\xa9\xc6\x93\x90'
 
 
     def testPack0(self):
@@ -103,10 +110,10 @@ class TestExpose(unittest.TestCase):
             'sequence_number': 45668,
             'height': 29709,
             }
-        self.evt_bin_0 = '\xc0\x00\x64\xb2' '\xb0\x95\xcc\x76' \
-            '\x24\x3d\xe2\x71' '\xc0\xde\x0d\x74' \
-            '\x57\x79\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xc0\x00\x64\xb2' b'\xb0\x95\xcc\x76' \
+            b'\x24\x3d\xe2\x71' b'\xc0\xde\x0d\x74' \
+            b'\x57\x79\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -142,10 +149,10 @@ class TestGraphicsExpose(unittest.TestCase):
             'sequence_number': 9516,
             'height': 10465,
             }
-        self.evt_bin_0 = '\x8a\x00\x2c\x25' '\xb1\xf4\xa7\x38' \
-            '\x79\xc3\x6c\x09' '\x92\x54\xe1\x28' \
-            '\x50\xad\x5a\x1b' '\xee\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\x8a\x00\x2c\x25' b'\xb1\xf4\xa7\x38' \
+            b'\x79\xc3\x6c\x09' b'\x92\x54\xe1\x28' \
+            b'\x50\xad\x5a\x1b' b'\xee\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -176,10 +183,10 @@ class TestNoExpose(unittest.TestCase):
             'major_event': 149,
             'sequence_number': 51301,
             }
-        self.evt_bin_0 = '\xc6\x00\x65\xc8' '\x22\x92\xd6\x52' \
-            '\xa2\xbf\x95\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xc6\x00\x65\xc8' b'\x22\x92\xd6\x52' \
+            b'\xa2\xbf\x95\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -209,10 +216,10 @@ class TestVisibilityNotify(unittest.TestCase):
             'state': 239,
             'sequence_number': 38248,
             }
-        self.evt_bin_0 = '\xe9\x00\x68\x95' '\x72\xac\x93\x32' \
-            '\xef\x00\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xe9\x00\x68\x95' b'\x72\xac\x93\x32' \
+            b'\xef\x00\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -248,10 +255,10 @@ class TestCreateNotify(unittest.TestCase):
             'sequence_number': 14268,
             'height': 8803,
             }
-        self.evt_bin_0 = '\xe6\x00\xbc\x37' '\x55\x6b\xb4\x06' \
-            '\x58\x8e\x2b\x4f' '\x94\xca\x74\x85' \
-            '\xef\x5f\x63\x22' '\x2c\x80\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xe6\x00\xbc\x37' b'\x55\x6b\xb4\x06' \
+            b'\x58\x8e\x2b\x4f' b'\x94\xca\x74\x85' \
+            b'\xef\x5f\x63\x22' b'\x2c\x80\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -281,10 +288,10 @@ class TestDestroyNotify(unittest.TestCase):
             'event': 1596763581,
             'sequence_number': 37839,
             }
-        self.evt_bin_0 = '\xb7\x00\xcf\x93' '\xbd\xad\x2c\x5f' \
-            '\x39\xd4\x86\x52' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xb7\x00\xcf\x93' b'\xbd\xad\x2c\x5f' \
+            b'\x39\xd4\x86\x52' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -315,10 +322,10 @@ class TestUnmapNotify(unittest.TestCase):
             'sequence_number': 55135,
             'from_configure': 0,
             }
-        self.evt_bin_0 = '\xc0\x00\x5f\xd7' '\x1a\x88\x73\x36' \
-            '\xf4\xb1\x87\x4b' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xc0\x00\x5f\xd7' b'\x1a\x88\x73\x36' \
+            b'\xf4\xb1\x87\x4b' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -349,10 +356,10 @@ class TestMapNotify(unittest.TestCase):
             'event': 1566597012,
             'sequence_number': 8920,
             }
-        self.evt_bin_0 = '\xd8\x00\xd8\x22' '\x94\x5f\x60\x5d' \
-            '\xe8\xb1\x5a\x77' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xd8\x00\xd8\x22' b'\x94\x5f\x60\x5d' \
+            b'\xe8\xb1\x5a\x77' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -382,10 +389,10 @@ class TestMapRequest(unittest.TestCase):
             'parent': 1188866605,
             'sequence_number': 6729,
             }
-        self.evt_bin_0 = '\xf2\x00\x49\x1a' '\x2d\xaa\xdc\x46' \
-            '\x4d\x6b\xba\x67' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xf2\x00\x49\x1a' b'\x2d\xaa\xdc\x46' \
+            b'\x4d\x6b\xba\x67' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -419,10 +426,10 @@ class TestReparentNotify(unittest.TestCase):
             'event': 1344092894,
             'sequence_number': 31034,
             }
-        self.evt_bin_0 = '\xb9\x00\x3a\x79' '\xde\x3a\x1d\x50' \
-            '\xff\xf9\xc4\x36' '\x1e\x3e\x65\x3e' \
-            '\xda\xd1\xfd\xd5' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xb9\x00\x3a\x79' b'\xde\x3a\x1d\x50' \
+            b'\xff\xf9\xc4\x36' b'\x1e\x3e\x65\x3e' \
+            b'\xda\xd1\xfd\xd5' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -459,10 +466,10 @@ class TestConfigureNotify(unittest.TestCase):
             'event': 2102634753,
             'sequence_number': 21818,
             }
-        self.evt_bin_0 = '\xbf\x00\x3a\x55' '\x01\xa9\x53\x7d' \
-            '\xe9\xba\x4c\x65' '\x29\x26\x2f\x44' \
-            '\x5f\xa3\xb9\x80' '\x7f\x5e\x4d\xad' \
-            '\x55\xca\x01\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xbf\x00\x3a\x55' b'\x01\xa9\x53\x7d' \
+            b'\xe9\xba\x4c\x65' b'\x29\x26\x2f\x44' \
+            b'\x5f\xa3\xb9\x80' b'\x7f\x5e\x4d\xad' \
+            b'\x55\xca\x01\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -500,10 +507,10 @@ class TestConfigureRequest(unittest.TestCase):
             'type': 140,
             'sequence_number': 48820,
             }
-        self.evt_bin_0 = '\x8c\x9b\xb4\xbe' '\xfc\xc8\x80\x58' \
-            '\xdd\x64\xbd\x20' '\xfe\xe2\xc1\x44' \
-            '\xfc\xd2\x05\xfc' '\x4a\xb6\x90\x6a' \
-            '\x53\xa1\x1b\xa3' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\x8c\x9b\xb4\xbe' b'\xfc\xc8\x80\x58' \
+            b'\xdd\x64\xbd\x20' b'\xfe\xe2\xc1\x44' \
+            b'\xfc\xd2\x05\xfc' b'\x4a\xb6\x90\x6a' \
+            b'\x53\xa1\x1b\xa3' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -535,10 +542,10 @@ class TestGravityNotify(unittest.TestCase):
             'event': 860169186,
             'sequence_number': 48472,
             }
-        self.evt_bin_0 = '\xbf\x00\x58\xbd' '\xe2\x23\x45\x33' \
-            '\x38\x1b\xb0\x57' '\x7e\xd5\x27\x97' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xbf\x00\x58\xbd' b'\xe2\x23\x45\x33' \
+            b'\x38\x1b\xb0\x57' b'\x7e\xd5\x27\x97' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -569,10 +576,10 @@ class TestResizeRequest(unittest.TestCase):
             'sequence_number': 9443,
             'height': 58942,
             }
-        self.evt_bin_0 = '\x8b\x00\xe3\x24' '\x73\xcf\x4f\x3b' \
-            '\x8a\x22\x3e\xe6' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\x8b\x00\xe3\x24' b'\x73\xcf\x4f\x3b' \
+            b'\x8a\x22\x3e\xe6' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -604,10 +611,10 @@ class TestPropertyNotify(unittest.TestCase):
             'state': 241,
             'sequence_number': 47586,
             }
-        self.evt_bin_0 = '\xcd\x00\xe2\xb9' '\xbe\x45\x1b\x69' \
-            '\x60\x2c\xd0\x02' '\xca\x79\xd2\x37' \
-            '\xf1\x00\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xcd\x00\xe2\xb9' b'\xbe\x45\x1b\x69' \
+            b'\x60\x2c\xd0\x02' b'\xca\x79\xd2\x37' \
+            b'\xf1\x00\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -638,10 +645,10 @@ class TestSelectionClear(unittest.TestCase):
             'sequence_number': 26660,
             'time': 1732839301,
             }
-        self.evt_bin_0 = '\xe8\x00\x24\x68' '\x85\x07\x49\x67' \
-            '\x51\x65\x0b\x14' '\xff\x27\x49\x0f' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xe8\x00\x24\x68' b'\x85\x07\x49\x67' \
+            b'\x51\x65\x0b\x14' b'\xff\x27\x49\x0f' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -675,10 +682,10 @@ class TestSelectionRequest(unittest.TestCase):
             'type': 147,
             'sequence_number': 20571,
             }
-        self.evt_bin_0 = '\x93\x00\x5b\x50' '\xe9\x35\xda\x54' \
-            '\xf3\x3e\x97\x2d' '\x41\xc6\xca\x0f' \
-            '\xc0\x1f\x8c\x5b' '\x07\xdb\x38\x24' \
-            '\x26\x99\x6e\x44' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\x93\x00\x5b\x50' b'\xe9\x35\xda\x54' \
+            b'\xf3\x3e\x97\x2d' b'\x41\xc6\xca\x0f' \
+            b'\xc0\x1f\x8c\x5b' b'\x07\xdb\x38\x24' \
+            b'\x26\x99\x6e\x44' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -711,10 +718,10 @@ class TestSelectionNotify(unittest.TestCase):
             'type': 133,
             'sequence_number': 30741,
             }
-        self.evt_bin_0 = '\x85\x00\x15\x78' '\xab\x44\xee\x3c' \
-            '\xb1\x59\xe8\x39' '\x06\x6d\x83\x13' \
-            '\xd1\xfe\xb7\x6f' '\xbe\x02\xcd\x6a' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\x85\x00\x15\x78' b'\xab\x44\xee\x3c' \
+            b'\xb1\x59\xe8\x39' b'\x06\x6d\x83\x13' \
+            b'\xd1\xfe\xb7\x6f' b'\xbe\x02\xcd\x6a' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -746,10 +753,10 @@ class TestColormapNotify(unittest.TestCase):
             'state': 168,
             'sequence_number': 8684,
             }
-        self.evt_bin_0 = '\xd3\x00\xec\x21' '\xbb\x4b\xb1\x50' \
-            '\x9d\xab\x52\x27' '\x01\xa8\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xd3\x00\xec\x21' b'\xbb\x4b\xb1\x50' \
+            b'\x9d\xab\x52\x27' b'\x01\xa8\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
@@ -780,10 +787,10 @@ class TestClientMessage(unittest.TestCase):
             'data': (8, '01234567890123456789'),
             'sequence_number': 14854,
             }
-        self.evt_bin_0 = '\xed\x08\x06\x3a' '\x82\xab\x90\x6b' \
-            '\x49\x39\x23\x1b' '\x30\x31\x32\x33' \
-            '\x34\x35\x36\x37' '\x38\x39\x30\x31' \
-            '\x32\x33\x34\x35' '\x36\x37\x38\x39'
+        self.evt_bin_0 = b'\xed\x08\x06\x3a' b'\x82\xab\x90\x6b' \
+            b'\x49\x39\x23\x1b' b'\x30\x31\x32\x33' \
+            b'\x34\x35\x36\x37' b'\x38\x39\x30\x31' \
+            b'\x32\x33\x34\x35' b'\x36\x37\x38\x39'
 
         self.evt_args_1 = {
             'type': 160,
@@ -792,10 +799,10 @@ class TestClientMessage(unittest.TestCase):
             'data': (16, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
             'sequence_number': 28171,
             }
-        self.evt_bin_1 = '\xa0\x10\x0b\x6e' '\x3e\xb2\x8e\x38' \
-            '\xac\x66\xa7\x0c' '\x01\x00\x02\x00' \
-            '\x03\x00\x04\x00' '\x05\x00\x06\x00' \
-            '\x07\x00\x08\x00' '\x09\x00\x0a\x00'
+        self.evt_bin_1 = b'\xa0\x10\x0b\x6e' b'\x3e\xb2\x8e\x38' \
+            b'\xac\x66\xa7\x0c' b'\x01\x00\x02\x00' \
+            b'\x03\x00\x04\x00' b'\x05\x00\x06\x00' \
+            b'\x07\x00\x08\x00' b'\x09\x00\x0a\x00'
 
         self.evt_args_2 = {
             'type': 243,
@@ -804,10 +811,10 @@ class TestClientMessage(unittest.TestCase):
             'data': (32, [1, 2, 3, 4, 5]),
             'sequence_number': 63569,
             }
-        self.evt_bin_2 = '\xf3\x20\x51\xf8' '\x46\x88\xaf\x22' \
-            '\xfe\x65\xa1\x39' '\x01\x00\x00\x00' \
-            '\x02\x00\x00\x00' '\x03\x00\x00\x00' \
-            '\x04\x00\x00\x00' '\x05\x00\x00\x00'
+        self.evt_bin_2 = b'\xf3\x20\x51\xf8' b'\x46\x88\xaf\x22' \
+            b'\xfe\x65\xa1\x39' b'\x01\x00\x00\x00' \
+            b'\x02\x00\x00\x00' b'\x03\x00\x00\x00' \
+            b'\x04\x00\x00\x00' b'\x05\x00\x00\x00'
 
 
     def testPack0(self):
@@ -874,10 +881,10 @@ class TestMappingNotify(unittest.TestCase):
             'count': 201,
             'sequence_number': 32665,
             }
-        self.evt_bin_0 = '\xc6\x00\x99\x7f' '\xbd\xf6\xc9\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00' \
-            '\x00\x00\x00\x00' '\x00\x00\x00\x00'
+        self.evt_bin_0 = b'\xc6\x00\x99\x7f' b'\xbd\xf6\xc9\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00' \
+            b'\x00\x00\x00\x00' b'\x00\x00\x00\x00'
 
 
     def testPack0(self):
