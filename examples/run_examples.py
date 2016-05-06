e9d552b49053b0d53030b610b5aecf1133e408ad
Svetlana Filicheva <filicheva.s@gmail.com>
2016-05-06 23:12:43 +0300

    Change paths

--- examples/run_examples.py
+++ examples/run_examples.py
@@ -30,11 +30,11 @@ from subprocess import call
 
 class TestExamples(unittest.TestCase):
 	def testEventthread(self):
-		self.assertEqual(subprocess.call("./eventthread.py"), 0)
+		self.assertEqual(subprocess.call("eventthread.py"), 0)
 
 	def testProfilex(self):
-		self.assertEqual(subprocess.call(["./profilex.py", "profilex_output"]), 0)
-		subprocess.call(["rm", "./profilex_output"])
+		self.assertEqual(subprocess.call(["profilex.py", "profilex_output"]), 0)
+		subprocess.call(["rm", "profilex_output"])
 
 
 if __name__ == '__main__':
