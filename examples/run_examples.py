013cfc0cbe0afdf76203e05201903514e350ec3c
Svetlana Filicheva <filicheva.s@gmail.com>
2016-05-06 22:06:21 +0300

    update license

--- examples/run_examples.py
+++ examples/run_examples.py
@@ -2,25 +2,31 @@
 #
 # examples/run_examples.py -- run some examples.
 #
-#	Copyright (C) 2008 David Bronke <whitelynx@gmail.com>
-#	Copyright (C) 2002 Peter Liljenberg <petli@ctrl-c.liu.se>
+# GUI Application automation and testing library
+# Copyright (C) 2016 Intel Corporation
+# Copyright (C) 2012 Michael Herrmann
+# Copyright (C) 2010 Mark Mc Mahon
 #
-#	This program is free software; you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation; either version 2 of the License, or
-#	(at your option) any later version.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 2.1
+# of the License, or (at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details.
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program; if not, write to the Free Software
-#	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the
+#    Free Software Foundation, Inc.,
+#    59 Temple Place,
+#    Suite 330,
+#    Boston, MA 02111-1307 USA
 
-import subprocess, unittest
-from subprocess import Popen, PIPE, call
+import subprocess
+import unittest
+from subprocess import call
 
 class TestExamples(unittest.TestCase):
 	def testEventthread(self):
