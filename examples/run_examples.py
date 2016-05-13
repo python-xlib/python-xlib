7b6359e173f96bcb5b2de69b4ef6d475003778f3
Svetlana Filicheva <filicheva.s@gmail.com>
2016-05-13 22:16:39 +0300

    Add docstring to run_examples.py

--- examples/run_examples.py
+++ examples/run_examples.py
@@ -32,6 +32,7 @@ import unittest
 examples_folder = os.path.abspath(os.path.dirname(__file__)) + "/"
 
 def run_example(path):
+    """ Returns returncode of example """
     proc = subprocess.Popen(path, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     res = proc.communicate()
     if proc.returncode:
