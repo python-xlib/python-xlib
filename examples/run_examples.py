10efe9513c79ab07d421d5f811e565e85f1d4f1b
Svetlana Filicheva <filicheva.s@gmail.com>
2016-05-14 02:25:29 +0300

    Uncomment test_eventthread

--- examples/run_examples.py
+++ examples/run_examples.py
@@ -42,10 +42,9 @@ def run_example(path):
 class TestExamples(unittest.TestCase):
     """ Run some of examples """
 
-# TODO
-#     def test_eventthread(self):
-#         """ Run eventthread.py -- Tests multithreaded event handling """
-#         self.assertEqual(run_example(examples_folder + "eventthread.py"), 0)
+    def test_eventthread(self):
+        """ Run eventthread.py -- Tests multithreaded event handling """
+        self.assertEqual(run_example(examples_folder + "eventthread.py"), 0)
 
     def test_get_selection(self):
         """ Run get_selection.py -- demonstrate getting selections """
