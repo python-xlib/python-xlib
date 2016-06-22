1f64ac04f2476319b6e99909ff639cfb9cc2c1ce
Sebastian Olsson <sebelino7@gmail.com>
2016-06-22 09:44:12 +0000

    Fixed broken subprocess calls when running Py2 with Py3 as the default.

--- examples/run_examples.py
+++ examples/run_examples.py
@@ -33,7 +33,8 @@ examples_folder = os.path.abspath(os.path.dirname(__file__)) + "/"
 
 def run_example(path):
     """ Returns returncode of example """
-    proc = subprocess.Popen(path, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    cmd = "{} {}".format(sys.executable, path)
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     res = proc.communicate()
     if proc.returncode:
         print(res[1].decode())
