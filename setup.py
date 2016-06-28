b86d84b470eb0b89c1898c1c49bcb1abdbc68c7f
Benoit Pierre <benoit.pierre@gmail.com>
2016-06-28 13:31:04 +0200

    setup: cosmetic

--- setup.py
+++ setup.py
@@ -5,26 +5,26 @@ from setuptools import setup
 import Xlib
 
 
-setup(name='python-xlib',
-        version=Xlib.__version_string__,
+setup(
+    name='python-xlib',
+    version=Xlib.__version_string__,
 
-        description='Python X Library',
-        download_url='https://github.com/python-xlib/python-xlib/releases',
-        url='https://github.com/python-xlib/python-xlib',
-        license='GPL',
+    description='Python X Library',
+    download_url='https://github.com/python-xlib/python-xlib/releases',
+    url='https://github.com/python-xlib/python-xlib',
+    license='GPL',
 
-        author='Peter Liljenberg',
-        author_email='petli@ctrl-c.liu.se',
+    author='Peter Liljenberg',
+    author_email='petli@ctrl-c.liu.se',
 
-        install_requires=['six>=1.10.0'],
+    install_requires=['six>=1.10.0'],
 
-        packages=[
-            'Xlib',
-            'Xlib.ext',
-            'Xlib.keysymdef',
-            'Xlib.protocol',
-            'Xlib.support',
-            'Xlib.xobject'
-            ],
-        )
-
+    packages=[
+        'Xlib',
+        'Xlib.ext',
+        'Xlib.keysymdef',
+        'Xlib.protocol',
+        'Xlib.support',
+        'Xlib.xobject'
+    ],
+)
