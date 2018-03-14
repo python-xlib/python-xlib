376b4739cedc4eee32e60e7b158d20ed18dfddc1
Benoit Pierre <benoit.pierre@gmail.com>
2018-03-14 22:57:30 +0100

    setup: check setuptools is recent enough to support `setup.cfg`

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 # Distutils script for python-xlib
 
-from setuptools import setup
+from pkg_resources import parse_requirements
+from setuptools import (__version__ as setuptools_version, setup)
+
+
+# Check setuptools is recent enough to support `setup.cfg`.
+setuptools_require = next(parse_requirements('setuptools>=30.3.0'))
+assert setuptools_version in setuptools_require, '{} is required'.format(setuptools_require)
 
 
 setup(
