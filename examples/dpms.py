733556f8b40b2d2643830ac193a0ffdd6560dbfc
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-09-14 14:34:32 -0300

    Fix e-mail in DPMS extension

--- examples/dpms.py
+++ examples/dpms.py
@@ -2,7 +2,7 @@
 #
 # examples/dpms.py -- DPMS usage examples.
 #
-#    Copyright (C) 2020 Thiago Kenji Okada<thiagokokada@gmail.com>
+#    Copyright (C) 2020 Thiago Kenji Okada <thiagokokada@gmail.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public License
