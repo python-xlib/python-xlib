b4f44528d9a2352bf60a333d03e71d6bfb26e8a0
Avasam <samuel.06@hotmail.com>
2022-11-18 20:57:05 -0500

    redundant _data def

--- Xlib/protocol/rq.py
+++ Xlib/protocol/rq.py
@@ -1374,7 +1374,6 @@ class ReplyRequest(GetAttrData):
         self._display = display
         self._binary = self._request.to_binary(*args, **keys)
         self._serial = None
-        self._data = {}  # type: dict[str, object]
         self._error = None
 
         self._response_lock = lock.allocate_lock()
